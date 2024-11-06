93403a1d51f48adfea0d449a3d3ee35a6e4fbce6
Chinh112 <ppchinh2314@gmail.com>
2024-11-06 22:18:51 +0700

    upload file

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,11 +12,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
+       
         private void label1_Click(object sender, EventArgs e)
         {
 
--- WindowsFormsApp1/WindowsFormsApp1/MainFrm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainFrm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,48 @@ namespace WindowsFormsApp1
 {
     public partial class MainFrm : Form
     {
+
+        private string connectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=baitaplon;User ID=sa;Password=12345";
+        SqlConnection conn = null;
         public MainFrm()
         {
             InitializeComponent();
+        }
+
+        private DataTable getData(String query)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var com = new SqlCommand(query, conn);
+                com.ExecuteNonQuery();
+                var dt = new DataTable();
+                var da = SqlDataAdapter(com);
+                da.Fill(dt);
+                conn.Close();
+                dtgvMonHoc.DataSource = dt;
+                return dt;
+            }
+        }
+
+      
+    
+
+        private object SqlDataAdapter(SqlCommand com)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool run(String query)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var com = new SqlCommand(query, conn);
+                com.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -35,6 +75,15 @@ namespace WindowsFormsApp1
         private void txtNoiDung_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            getData("select*from Khoa");
+            getData("select*from SinhVien");
+            getData("select*from MonHoc");
+            getData("select*from KetQua");
+            getData("select*from DangNhap");
         }
     }
 }
