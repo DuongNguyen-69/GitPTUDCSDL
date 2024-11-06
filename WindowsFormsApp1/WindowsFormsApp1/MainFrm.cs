c6d8f51f2d8be46aba4c08d5a1a9be3916aa0461
Chinh112 <ppchinh2314@gmail.com>
2024-11-07 00:10:07 +0700

    upload file

--- WindowsFormsApp1/WindowsFormsApp1/MainFrm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainFrm.cs
@@ -1,89 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class MainFrm : Form
     {
+        private string connectionString = "Data Source=MSI\SQLEXPRESS;Initial Catalog=baitaplon;User ID=sa;Password=12345";
 
-        private string connectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=baitaplon;User ID=sa;Password=12345";
-        SqlConnection conn = null;
         public MainFrm()
         {
             InitializeComponent();
         }
 
-        private DataTable getData(String query)
+        private DataTable getData(string query)
         {
             using (var conn = new SqlConnection(connectionString))
             {
-                conn.Open();
-                var com = new SqlCommand(query, conn);
-                com.ExecuteNonQuery();
-                var dt = new DataTable();
-                var da = SqlDataAdapter(com);
-                da.Fill(dt);
-                conn.Close();
-                dtgvMonHoc.DataSource = dt;
-                return dt;
+                try
+                {
+                    conn.Open();
+
+                    var da = new SqlDataAdapter(query, conn);
+
+                    var dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dtgvMonHoc.DataSource = dt;
+
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Có lỗi xảy ra: {ex.Message}");
+                    return null;
+                }
             }
         }
 
-      
-    
-
-        private object SqlDataAdapter(SqlCommand com)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool run(String query)
+        private bool run(string query)
         {
             using (var conn = new SqlConnection(connectionString))
             {
-                conn.Open();
-                var com = new SqlCommand(query, conn);
-                com.ExecuteNonQuery();
-                conn.Close();
-                return true;
+                try
+                {
+                    conn.Open();
+                    var com = new SqlCommand(query, conn);
+                    com.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Có lỗi xảy ra: {ex.Message}");
+                    return false;
+                }
             }
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNoiDung_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void MainFrm_Load(object sender, EventArgs e)
         {
-            getData("select*from Khoa");
-            getData("select*from SinhVien");
-            getData("select*from MonHoc");
-            getData("select*from KetQua");
-            getData("select*from DangNhap");
+            getData("SELECT * FROM Khoa");
+            getData("SELECT * FROM SinhVien");
+            getData("SELECT * FROM MonHoc");
+            getData("SELECT * FROM KetQua");
+            getData("SELECT * FROM DangNhap");
         }
+
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void txtNoiDung_TextChanged(object sender, EventArgs e) { }
     }
 }
